021976252a92d9b0debb7a7e881f9ac737d2a29f
DrissOubah <drissoubahmane@gmail.com>
2023-10-24 18:25:46 -0700

    sort

--- sort.h
+++ sort.h
@@ -1,21 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
-#include <stddef.h>
 #include <stdlib.h>
-
-/* Print Functions */
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-
-/* Sorting Algorithms Functions */
-
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-void bubble_sort(int *array, size_t size);
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -30,4 +16,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 #endif /* SORT_H */
