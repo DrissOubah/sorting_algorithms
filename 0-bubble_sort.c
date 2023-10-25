9593dc5e7f16a6a29a8dc6c6d403729bb28f274a
DrissOubah <drissoubahmane@gmail.com>
2023-10-24 17:51:31 -0700

    Bubblt Sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,27 +5,28 @@
  * bubble_sort - Sorts an array of integers
  * in ascending order using Bubble Sort.
  *
- * @array: The array to be sorted.
+ * @array: The array to be sortty.
  * @size: The size of the array.
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
+	size_t i;
+	int temp, sortty;
 
-	if (array == NULL || size < 2)
+	if (!array || size == 0)
 		return;
-	for (i = 0; i < size - 1; i++)
-	{
-		for (j = 0; j < size - i - 1; j++)
+	do {
+		sortty = 1;
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				sortty = 0;
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
 				print_array(array, size);
 			}
 		}
-	}
+	}while (sortty == 0);
 }
