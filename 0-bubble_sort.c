d9b183faf8ac108540a048bc24ed8d44e57c1b32
DrissOubah <drissoubahmane@gmail.com>
2023-10-24 18:23:13 -0700

    Bubble Sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,33 +1,25 @@
 #include "sort.h"
-
 /**
- * bubble_sort - Sorts an array of integers
- * in ascending order using Bubble Sort.
- *
- * @array: The array to be sortty.
- * @size: The size of the array.
- * Return : void
+ * bubble_sort - sort array lements from min to max value
+ * @array: array
+ * @size: array size
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, lenght = size;
-	bool bubbles = false;
 
-	if (array == NULL || size < 2)
+	size_t i, index, tmp = 0;
+
+	if (size < 2)
 		return;
-
-	while (bubbles == false)
-	{
-		bubbles = true;
-		for (i = 0; i < lenght - 1; i++)
+	for (i = 0; i < size; i++)
+		for (index = 0; index < size; index++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[index] > array[index + 1] && array[index + 1])
 			{
-				swap_ints(array + i, array + i + 1);
-				print_array(array, size);
-				bubbles = false;
+			tmp = array[index];
+			array[index] = array[index + 1];
+			array[index + 1] = tmp;
+			print_array(array, size);
 			}
 		}
-		lenght--;
-	}
 }
