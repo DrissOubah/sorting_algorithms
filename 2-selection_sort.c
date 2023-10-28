940efd5c9239119118326fcd74ee41e64453fbf2
DrissOubah <drissoubahmane@gmail.com>
2023-10-28 16:09:52 -0700

    2. Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,14 +7,17 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j, min_index;
+	int temp;
+
 	if (array == NULL || size < 2)
 		return;
 
-	for (size_t i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		size_t min_index = i;
+		min_index = i;
 
-		for (size_t j = i + 1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
 				min_index = j;
@@ -22,7 +25,7 @@ void selection_sort(int *array, size_t size)
 
 		if (min_index != i)
 		{
-			int temp = array[i];
+			temp = array[i];
 
 			array[i] = array[min_index];
 			array[min_index] = temp;
